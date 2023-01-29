306d92e1e23ef14d4c8356f4165417c73f18bda6
JoannaT07 <joannatyminska@onet.pl>
2023-01-29 11:46:57 +0100

    changes to the ingredients service, refactoring code

--- server/src/models/types.ts
+++ server/src/models/types.ts
@@ -1,22 +1,23 @@
+export interface IngredientsConfig {
+  forbiddenValues: string[];
+  ingredients: BasicIngredient[];
+}
+
+export interface BasicIngredient {
+  id: string;
+  name: string;
+  searchValue?: string;
+}
+
 export interface RawRecipe {
   name: string | null;
   ingredients: RawIngredient[];
   instructions: string[] | null;
   image: string | null;
+  tags: string | null;
 }
 
 export type RawIngredient = {
   name: string;
   quantity: string;
 };
-
-export type Ingredient = {
-  id: string;
-  quantity: string;
-};
-
-export interface Recipe {
-  name: string | null;
-  ingredients: Ingredient[];
-  instructions: string[] | null;
-}
--- server/src/routes/recipes.ts
+++ server/src/routes/recipes.ts
@@ -1,15 +1,25 @@
 import { Request, Response, Router } from "express";
 import RecipeModel from "../models/Recipes";
-// import {processRawRecipes} from "./recipesSource";
 const router = Router();
 
-router.get('/', async (req: Request, res: Response) => {
-    try {
-        const recipes = await RecipeModel.find()
-        res.json(recipes)
-    }catch(err){
-        res.json({message: err})
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    const ingredient = req.query.ingredients as string;
+    if (ingredient) {
+      const query = ingredient.split(",").map((ingredient) => ({
+        $elemMatch: { id: ingredient },
+      }));
+      const recipes = await RecipeModel.find({
+        ingredients: { $all: query },
+      });
+      res.json(recipes);
+    } else {
+      const recipes = await RecipeModel.find();
+      res.json(recipes);
     }
-})
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
 
 export default router;
