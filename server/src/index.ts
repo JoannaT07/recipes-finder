363e2dfa6e12455414b076911bc04867097c043c
JoannaT07 <joannatyminska@onet.pl>
2023-02-19 18:24:36 +0100

    Adding static content serve

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,6 +19,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/ingredients", ingredientsRouter);
 app.use("/api/recipes", recipesRouter);
+app.use(express.static('./static'));
 
 main();
 processRawRecipes();
